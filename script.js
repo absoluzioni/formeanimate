f1f5bb908151509adf0ac29130aa0d5574b9284b
absoluzioni <absoluzioni@gmail.com>
2023-12-23 23:27:48 +0100

    perspective change from card to message

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let htmlSymbol = document.querySelector(".ball--2");
 let piano = document.querySelector(".shape");
 let filterElements = document.querySelectorAll('[class *= "filter-"]');
 let filterImages= document.querySelectorAll(".container-image");
+let retro = document.querySelector(".retro");
 
 console.log(filterElements.length);
 ball.addEventListener("click", function() {
@@ -20,10 +21,27 @@ htmlSymbol.addEventListener("click", function() {
 })
 
 piano.addEventListener("click", function() {
-    piano.classList.add("animation-3");
-    setTimeout(function() {
-        piano.classList.remove("animation-3");
-    }, 3001);
+    if (piano.classList.contains("view-card")) {
+        piano.classList.add("animation-3");
+        retro.classList.add("animation-text");
+        setTimeout(function() {
+            piano.classList.remove("view-card");
+            piano.classList.remove("animation-3");
+            retro.classList.remove("animation-text");
+            piano.classList.add("view-message");
+            retro.classList.add("view-text");
+        }, 2001)
+    } else {
+        piano.classList.add("animation-4");
+        retro.classList.remove("view-text");
+        retro.classList.add("animation-removetext");
+        setTimeout(function() {
+            piano.classList.remove("view-message");
+            piano.classList.remove("animation-4");
+            retro.classList.remove("animation-removetext");
+            piano.classList.add("view-card");
+        }, 2001)
+    }
 })
 
 
@@ -39,7 +57,16 @@ filterImages.forEach(function(element) {
             myImage.classList.remove("filter-none");
             description.classList.remove("no-display");
         }
-        
+    });
+});
+
+filterElements.forEach(function(element) {
+    element.addEventListener("click", function(e) {
+        if (!e.target.classList.contains("filter-none")) {
+            e.target.classList.add("filter-none");
+        } else {
+            e.target.classList.remove("filter-none");
+        }
     });
 });
 
